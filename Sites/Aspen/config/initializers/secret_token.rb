e95076d96413ee2297d46122d2aeeb0ece4658a0
SebastianCastiblanco <sebastiancasti@gmail.com>
2013-05-01 08:02:15 -0500

    commit inicial

---
+++ Sites/Aspen/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aspen::Application.config.secret_token = '0d641a9d16b729fafcf49395de53d8ed166f89e300dbf61a922941609a69d276c8d9f0a267d80cace92282bf40860469b159d07df64f19e30a77f68a1e19dc89'
